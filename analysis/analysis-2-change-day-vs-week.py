ec267d001da203f19e70037432afb89d1d3dbb50
darky <larsrotgers@gmail.com>
2017-07-27 15:32:35 +0200

    'commit'

--- analysis/analysis-2-change-day-vs-week.py
+++ analysis/analysis-2-change-day-vs-week.py
@@ -2,11 +2,8 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import coinmarketcap
 
-print('Retrieving data...')
 data = coinmarketcap.retrieve_data()
-
-print('Generating plot...')
-series = [(-1.5,-5.6,'DGB'), (10,-5,'XRP'), (5,3,'BTC'), (12,-3,'PIVX')]
+series = [(d["percent_change_24h"], d["percent_change_7d"], d["symbol"]) for d in data if d["percent_change_24h"] != None and d["percent_change_7d"] != None and int(d["rank"]) < 50]
 x, y, z = zip(*series)
 
 trace0 = go.Scatter(
@@ -25,17 +22,16 @@ layout = go.Layout(
     title = 'Change in % by Coin (top: 50)',
     hovermode='closest',
     xaxis=dict(
-        title='% Change (1-hour)'
+        title='% Change (24-hours)'
     ),
     yaxis=dict(
-        title='% Change (24-hour)'
+        title='% Change (7-days)'
     ),
     showlegend = False
 )
 
 fig = go.Figure(data=data, layout=layout)
+#py.image.save_as(fig, filename='plot.png')
+py.plot(fig, filename='cbc2t50')
 
-print('Saving to file...')
-py.image.save_as(fig, filename='plot.png')
 
-print('Saved as plot.png')
--- analysis1.py
+++
@@ -1,5 +0,0 @@
-point = (5, 10)
-
-print( point) 
-
-point[0]
